79e704db79589be5f2e149ad3853caed895e91c4
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-01 16:41:25 +0200

    Last Digits Python

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ string0 = 'and is 0 '
 string6 = 'and is less than 6 and not 0'
 ldigit = abs(number) % 10
 
-if ldigit > 5 :
+if ldigit > 5:
     print(f"{string1} {number} {string2} {ldigit} {string5}")
-elif ldigit == 0 : 
+elif ldigit == 0:
     print(f"{string1} {number} {string2} {ldigit} {string0}")
-elif ldigit != 0 and ldigit > 6 :
+elif ldigit != 0 and ldigit > 6:
     print(f"{string1} {number} {string2} {ldigit} {string6}")
