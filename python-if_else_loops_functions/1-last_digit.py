d35a9c5ae925eb6f2a200428b469bc4fb5487e66
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-01 16:38:01 +0200

    Last Digits Python

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+string1 = 'Last digit of '
+string2 = 'is'
+string5 = 'and is greater than 5'
+string0 = 'and is 0 '
+string6 = 'and is less than 6 and not 0'
+ldigit = abs(number) % 10
+
+if ldigit > 5 :
+    print(f"{string1} {number} {string2} {ldigit} {string5}")
+elif ldigit == 0 : 
+    print(f"{string1} {number} {string2} {ldigit} {string0}")
+elif ldigit != 0 and ldigit > 6 :
+    print(f"{string1} {number} {string2} {ldigit} {string6}")
