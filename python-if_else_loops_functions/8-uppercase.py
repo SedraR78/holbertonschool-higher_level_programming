c702d10097face833b288be6d1b76b6f5cde90c8
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-03 01:19:44 +0200

    Task 10 addition

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 def uppercase(str):
-    result=""
+    result = ""
     for char in str:
-        if 97<= ord(char) <= 122:
+        if 97 <= ord(char) <= 122:
             result += chr(ord(char) - 32)
         else:
             result += char
     print(result)
-    return(result)
+    return (result)
