ef2a5f4ca7dd68319f4b78430efe7800f1738f05
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-02 20:09:49 +0200

    is upper

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    result=""
+    for char in str:
+        if 97<= ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
