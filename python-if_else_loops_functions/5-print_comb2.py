35c57477aac9defd9b675db232d0b4e4cf8b7407
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-02 11:20:25 +0200

    print combo

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for numberOne in range(0, 9):
+    for numberTwo in range(0, 9):
+        if numberOne != numberTwo and numberOne < numberTwo :
+            print(f"{numberOne}{numberTwo} ,", end="")
