152f0fa55806a2748e4d88f4089959db03d82395
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-03 02:43:44 +0200

    pycodestyle fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if 0 <= num < 10:
-        print(f"0{num} ,", end="")
+        print(f"0{num}, " , end="")
+    elif num == 99:
+        print(f"{num}")
     else:
-        print(f"{num} ,", end="")
+        print(f"{num}, " , end="")
