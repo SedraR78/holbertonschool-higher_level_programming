0959f7697a6524a48e11ad652db3f32c259dc175
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-09 12:02:08 +0200

    task 7 adding Tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+     # Completes tuples with zeros if necessary
+    a1, a2 = (tuple_a + (0, 0))[:2]  # Limit to 2 elements
+    b1, b2 = (tuple_b + (0, 0))[:2]  # Limit to 2 elements
+
+    # Adds the first and second elements
+    return (a1 + b1, a2 + b2)
