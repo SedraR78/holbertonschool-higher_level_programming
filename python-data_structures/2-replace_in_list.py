15842c672171ee9718c29a11fb684f15ed0bda5b
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-09 23:12:06 +0200

    pycodestyle

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx > 0 and idx < len(my_list):
-        my_list[idx] = element
+    # Vérifier si l'index est négatif ou hors de portée
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    # Remplacer l'élément à la position idx
+    my_list[idx] = element
     return my_list
- 
