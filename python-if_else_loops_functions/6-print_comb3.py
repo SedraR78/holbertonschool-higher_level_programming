ca43dc9b5002c0ef47efb8d101f517654c0b1872
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-10 09:47:51 +0200

    adding string format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 for numberOne in range(0, 10):
     for numberTwo in range(0, 10):
         if numberOne != numberTwo and numberOne < numberTwo:
-            print(f"{numberOne}{numberTwo}, ", end="")
+            if numberOne == 8 and numberTwo == 9:
+                print("{}{}".format(numberOne, numberTwo))
+            else:
+                print("{}{}, ".format(numberOne, numberTwo), end="")
