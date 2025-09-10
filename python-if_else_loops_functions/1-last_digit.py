770c9dfa3dba488e3ba13b1a8b854c807fc7e3f1
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-10 10:49:55 +0200

    fixing code

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 
 string1 = 'Last digit of'
@@ -7,19 +8,13 @@ string2 = 'is'
 string5 = 'and is greater than 5'
 string0 = 'and is 0'
 string6 = 'and is less than 6 and not 0'
-sl = "\n"
 
-ldigit = number % 10  # Get the last digit, keeping the sign
+# Récupération du dernier chiffre signé
+ldigit = number % 10 if number >= 0 else number % -10
 
-# Determine the correct display value for ldigit
-if number < 0 and ldigit != 0:
-    ldigit_display = -ldigit
+if ldigit > 5:
+    print(f"{string1} {number} {string2} {ldigit} {string5}")
+elif ldigit == 0:
+    print(f"{string1} {number} {string2} {ldigit} {string0}")
 else:
-    ldigit_display = ldigit
-
-if ldigit_display > 5:
-    print(f"{string1} {number} {string2} {ldigit_display} {string5}")
-elif ldigit_display == 0:
-    print(f"{string1} {number} {string2} {ldigit_display} {string0}")
-else:  # When ldigit_display < 6
-    print(f"{string1} {number} {string2} {ldigit_display} {string6}")
+    print(f"{string1} {number} {string2} {ldigit} {string6}")
