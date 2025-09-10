7ba635b2774b5844adefb33470c4b16c8cffab66
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-10 10:35:20 +0200

    adding string format

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,18 +4,15 @@ number = random.randint(-10000, 10000)
 string1 = 'Last digit of'
 string2 = 'is'
 string5 = 'and is greater than 5'
-string0 = 'and is 0 '
+string0 = 'and is 0'
 string6 = 'and is less than 6 and not 0'
 sl = "\n"
-ldigit = abs(number) % 10
-minus = "-"
-if ldigit > 5:
-    print(f"{string1} {number} {string2} {ldigit} {string5}")
-elif ldigit == 0:
-    print(f"{string1} {number} {string2} {ldigit} {string0}")
-elif ldigit != 0 and ldigit < 6:
-    print(f"{string1} {number} {string2} {ldigit} {string6}")
-elif ldigit < 0:
-    print(f"{string1} {number} {string2} {minus} {ldigit} {string6}")
 
- 
+ldigit = number % 10  # On prend le dernier chiffre tel quel, sans abs()
+
+if ldigit > 5:
+    print(f"{string1} {number} {string2} {ldigit} {string5}{sl}")
+elif ldigit == 0:
+    print(f"{string1} {number} {string2} {ldigit} {string0}{sl}")
+else:  # ldigit < 6
+    print(f"{string1} {number} {string2} {ldigit} {string6}{sl}")
