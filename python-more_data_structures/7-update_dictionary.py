5a9969ca7b1afb3f853f9da091712f975b1cdc62
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-11 13:13:10 +0200

    Task 7 : updatete dictionary ù

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value  # Replace or add the key-value pair
+    return a_dictionary  # Return the updated dictionary
