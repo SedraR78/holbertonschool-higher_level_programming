af47af7a231948e92789b44594aca5d562308abf
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-11 10:59:48 +0200

    a function that returns a set of common elements in two sets.

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3`
-def uniq_add(my_list=[]):
-    total = 0
-    unique_numbers = []  # This list will keep track of unique integers
-    for number in my_list:
-        if number not in unique_numbers:
-            unique_numbers.append(number)  # Add to unique list
-            total += number  # Add to total
-    return total
-
+def common_elements(set_1, set_2):
+    common_set = set()
+    for i in (set_1):
+        if i in (set_2):
+            common_set.add(i)    
+    return common_set
