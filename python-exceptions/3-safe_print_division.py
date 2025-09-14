419475b4a79360e3102dd9cf3f09340e1e2efb69
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-14 21:34:29 +0200

    pycodestyle

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,6 +4,6 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result: {}".format(result))
+        print("Inside result: {:d}".format(result))
     
     return result
