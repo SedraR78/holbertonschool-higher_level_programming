c4fae437623bd441b7b6ec74b7211b73b79e1206
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-15 11:29:12 +0200

    Task 2 Square(self,size)

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a square.
+
+    This class currently has no properties or methods.
+    It can be extended later to include specific functionalities
+    related to a square.
+    """
+    def __init__(self,size):
+        self.__size = size
+
+    def get_size(self):
+        return self
