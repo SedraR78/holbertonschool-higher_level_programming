f0aba360154a83280f86c8230acf7290f42ffc0d
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-19 12:33:44 +0200

    creation of the empty class Rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""  class Rectangle """
+
+class Rectangle:
+    """Represents a square.
+
+    This class currently has no properties or methods.
+    It can be extended later to include specific functionalities
+    related to a square.
+    """
+    pass
