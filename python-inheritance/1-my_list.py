64ddf9f69a5345394fd64a54e72f906d908785f0
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-22 10:57:46 +0200

    adding documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Task 1 : My List """
 class MyList(list):
     def print_sorted(self):
+        """ Function that print a list of the instances of a given class """
         print (sorted(self))
