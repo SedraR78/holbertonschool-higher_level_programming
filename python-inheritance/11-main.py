8670c969fad157b94e7cb28305f9dfcafb66f130
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-23 10:57:44 +0200

    Square 2

--- python-inheritance/11-main.py
+++ python-inheritance/11-main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 Square = __import__('11-square').Square
 
 s = Square(13)
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -8,19 +8,6 @@ Square = __import__('10-square').Square
 
 """create a subclasse Square of BaseGeometry """
 
-class Square(Rectangle):
-    """Represents a Square"""
-
-    def __init__(self, size):
-        """ setting  size in private instance"""
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
-
-    def area(self):
-        """Return the area of the Square."""
-        return self.__size * self.__size
-
-    def __str__(self):
-        """Return a string representation of the rectangle."""
-        return f"[Square] {self.__size}/{self.__size}"
+def __str__(self):
+    """Return a string representation of the rectangle."""
+    return f"[Square] {self.__size}/{self.__size}"
