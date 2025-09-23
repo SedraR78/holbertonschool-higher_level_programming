ed35ebd42a2385d0b66a5645c9cf29e55499826a
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-23 11:22:18 +0200

    Test Files

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Task 1 : My List """
 class MyList(list):
+    """Create the class"""
     def print_sorted(self):
         """ Function that print a list of the instances of a given class """
         print(sorted(self))
