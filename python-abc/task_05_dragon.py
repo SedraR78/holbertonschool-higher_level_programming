cd506afad06e67a3d5793665f38beb19f46e7069
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-28 19:09:29 +0200

    pycodestyle

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
 """Creating Parent Classes"""
+
+
 class SwimMixin:
     def swim(self):
         print("The creature swims!")
+
 
 class FlyMixin:
     def fly(self):
         print("The creature Flies!")
 
+
 """Using Mixin"""
-class Dragon(SwimMixin,FlyMixin):
+
+
+class Dragon(SwimMixin, FlyMixin):
     def roar(self):
         print("The dragon roars!")
-
