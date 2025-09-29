f657acd1deddf2b51e4044a0b9a0ced12415f318
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-30 01:17:08 +0200

    pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
-#!usr/bin/python3
-"""Function that writes a file """
+#!/usr/bin/python3
+"""Function that writes a file."""
 
 
 def write_file(filename="", text=""):
-   """ Function that writes a string to a text files """
-   with open (filename,'w' ,encoding='utf-8') as f:
+    """Function that writes a string to a text file."""
+    with open(filename, 'w', encoding='utf-8') as f:
         content = f.write(text)
-        return content
+    return content
