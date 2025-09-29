259ee88505856b2faf9d3c9ac233f8953f21e9e3
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-29 18:57:24 +0200

    pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,9 @@
-#!usr/bin/python3
-
+#!/usr/bin/python3
 """Reads and prints the content of a UTF-8 text file"""
 
 
 def read_file(filename=""):
     """Open a file and print its content to stdout"""
-    with open (filename, encoding='utf-8') as f:
-        content = f.read()
-    print(content, end='')
-
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data, end="")
