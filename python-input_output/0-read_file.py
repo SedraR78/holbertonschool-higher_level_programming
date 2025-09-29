e263ebb8adaf2489587c427f2e5c8514f7674bf0
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-29 17:08:21 +0200

    pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 
 def read_file(filename=""):
     """Open a file and print its content to stdout"""
-    with open (filename,'r' ,encoding='utf-8') as file:
+    with open (filename, encoding='utf-8') as file:
         content = file.read()
     print(content, end='')
