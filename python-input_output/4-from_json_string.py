cafacb1f2d97c68409c344d86f8a5a701a3ee20c
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-29 19:01:34 +0200

    pycodestyle

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-import json
+#!/usr/bin/python3
+"""Append a string to a text file and return char count"""
 
-def from_json_string(my_str):
-    return json.loads(my_str)
+
+def append_write(filename="", text=""):
+    """Append text and return number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
