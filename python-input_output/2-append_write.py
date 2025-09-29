7a2d866790e034d3c747ae4ee400d69f00fc6338
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-30 01:31:53 +0200

    pycodestyle

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,6 +5,6 @@
 
 def append_write(filename="", text=""):
 
-    with open(filename, 'a+', encoding='utf-8') as f:
+    with open(filename, 'a', encoding='utf-8') as f:
         content = f.write(text)
         return content
