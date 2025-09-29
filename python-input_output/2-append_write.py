527939afcde19494af32f61fd3b3292d77592a52
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-30 01:28:55 +0200

    pycodestyle

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+"""Append a string at the end of a txt file return the nb of char."""
+
+
 def append_write(filename="", text=""):
 
-    with open (filename,'a' ,encoding='utf-8') as f:
+    with open(filename, 'a+', encoding='utf-8') as f:
         content = f.write(text)
         return content
