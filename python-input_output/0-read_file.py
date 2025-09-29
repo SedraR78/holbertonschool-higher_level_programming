11ff38c74aac8cd6c2420a77585c66a73672a74a
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-29 17:06:09 +0200

    JSON

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!usr/bin/python3
 
+"""Reads and prints the content of a UTF-8 text file"""
+
 
 def read_file(filename=""):
+    """Open a file and print its content to stdout"""
     with open (filename,'r' ,encoding='utf-8') as file:
         content = file.read()
     print(content, end='')
