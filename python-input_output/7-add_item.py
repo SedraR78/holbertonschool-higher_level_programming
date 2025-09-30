84fc65396072579d7bbd8f940a7c5157723ab9ce
Sedra Ramarosaona <sedranante@gmail.com>
2025-09-30 12:10:06 +0200

    adds all arguments to a Python list, and then save them to a file:

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """JSON file to store the list"""
 filename = "add_item.json"
 
-"""Load existing list, or start empty if file not found"""
+"""If the file doesn t exist, it should be created"""
 try:
     my_list = load_from_json_file(filename)
 except FileNotFoundError:
