24f349a7666525e80bfe3a2d28ca83725fabd619
Sedra Ramarosaona <sedranante@gmail.com>
2025-10-06 17:45:58 +0200

    task 02

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -25,15 +25,13 @@ def convert_csv_to_json(csv_filename):
             for row in csv_reader:
                 data.append(row)
         
-        # Serialize and write JSON data
+        # Serialize and write JSON data to data.json
         with open('data.json', 'w', encoding='utf-8') as json_file:
             json.dump(data, json_file, indent=4)
         
         return True
         
     except FileNotFoundError:
-        print(f"Error: File '{csv_filename}' not found.")
         return False
-    except Exception as e:
-        print(f"Error during conversion: {e}")
+    except Exception:
         return False
