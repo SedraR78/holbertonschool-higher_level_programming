7b5e81e82a5adae2fb7dad55d8500f9e8ed32c1c
Sedra Ramarosaona <sedranante@gmail.com>
2025-10-06 13:14:35 +0200

    TasK 1 serialization

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+import pickle
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age 
+        self.is_student = is_student
+
+    def display(self):
+        print(f"{self.name}  {self.age} , {self.is_student}")
+
+
+    def serialize_and_save_to_file(data, filename):
+
+        with open(filename, 'wb') as file:
+            pickle.dump(data, file)
