73a668f4bdbd613483eba378c7e8f4db7c554d70
Sedra Ramarosaona <sedranante@gmail.com>
2025-10-06 12:42:02 +0200

    Task 1 serialize and save to file

--- python-serialization/00_main.py
+++ python-serialization/00_main.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python3
-import pickle
-
-def serialize_and_save_to_file(data, filename):
-
-    with open(filename, 'wb') as file:
-        pickle.dump(data, file)
-
-def load_and_deserialize(filename):
-    with open(filename, 'rb') as file:
-        return pickle.load(file)
-
+from task_00_basic_serialization import load_and_deserialize, serialize_and_save_to_file
 
 # Sample data to be serialized
 data_to_serialize = {
