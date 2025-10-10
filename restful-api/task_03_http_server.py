47efd1e6056483ddf09446b43c720d091cc463bf
Sedra Ramarosaona <sedranante@gmail.com>
2025-10-10 12:36:29 +0200

    Task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -34,8 +34,7 @@ class MyHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write(b'Endpoint not found')
 
 if __name__ == "__main__":
     server_address = ('', 8000) 
