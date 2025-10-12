6763dbd532a861526392e2fb492f04775c4db8d7
Sedra Ramarosaona <sedranante@gmail.com>
2025-10-12 23:48:52 +0200

    4. Develop a Simple API using Python with Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,48 +1,86 @@
 from flask import Flask, jsonify, request
 
+# Create a Flask application
 app = Flask(__name__)
 
+# User dictionary for tests :
 users = {}
 
-@app.route('/')
+
+@app.route("/")
 def home():
+    """
+    Home Default root URL
+
+    Will return a simple message
+    """
     return "Welcome to the Flask API!"
 
-@app.route('/status')
-def get_status():
-    return jsonify({"status": "OK"})
 
-@app.route('/users/<username>')
+@app.route("/data")
+def get_data():
+    """
+    get_data
+
+    Get data from /data
+
+    Returns:
+       dict: A dictionary containing the usernames
+    """
+
+    return jsonify(list(users.keys()))
+
+
+@app.route("/status")
+def get_status():
+    """
+    get_status
+    Get status from /status
+
+    Returns:
+        str : OK
+    """
+    return "OK"
+
+
+@app.route("/users/<username>")
 def get_user(username):
-    user_data = users.get(username)
-    if user_data:
-        return jsonify(user_data)
+    """
+    get_user
+
+    Args:
+        username (str): username to get
+
+    Returns:
+        user data
+    """
+    if username in users:
+        return jsonify(users[username])
     else:
         return jsonify({"error": "User not found"}), 404
 
-@app.route('/data')
-def get_data():
-    list_usernames = list(users.keys())
-    return jsonify(list_usernames)
 
-@app.route('/add_user', methods=['POST'])
+@app.route("/add_user", methods=["POST"])
 def add_user():
-    if not request.is_json:
-        return jsonify({"error": "Data must be in JSON format"}), 415
-    
-    user_data = request.get_json()
-    
-    if 'username' not in user_data:
-        return jsonify({"error": "'username' field is required"}), 400
-    
-    username = user_data['username']
-    
-    users[username] = user_data
-    
-    return jsonify({
-        "message": "User added successfully",
-        "user": user_data
-    }), 201
+    """
+    add_user
+
+    add user with POST method
+
+    Returns:
+       user data successfully added
+    """
+    retrieved_data = request.get_json()
+
+    # Check if dictionnary contains a username key
+    if not retrieved_data or "username" not in retrieved_data:
+        return jsonify({"error": "Username is required"}), 400
+
+    username = retrieved_data["username"]
+    users[username] = retrieved_data
+
+    return jsonify({"message": "User added", "user": users[username]}), 201
+
 
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
