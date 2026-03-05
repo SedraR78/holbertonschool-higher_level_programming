31277e37d4e9899ffefae0b28f37a773dbe57d6d
Sedra Ramarosaona <sedranante@gmail.com>
2026-03-05 09:48:54 +0100

    Getting The Average of Values in a Table

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
--- AVG is also working btw but just we get a different output 
-SELECT as(score) FROM second_table;
+-- only AVG is also working btw but just we get a different output
+SELECT AVG(score)AS average FROM second_table;
