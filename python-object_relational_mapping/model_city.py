efe6ae01dbd5f82f6c443dc81afeac46dc475457
Sedra Ramarosaona <sedranante@gmail.com>
2026-04-08 22:33:13 +0200

    python-object_relational_mapping: task 14
    - model_city.py: City class with SQLAlchemy
    - 14-model_city_fetch_by_state.py: fetch cities by state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,4 +1,12 @@
-python-object_relational_mapping: task 14
+#!/usr/bin/python3
+"""Module that defines the City class"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
 
-- model_city.py: City class with SQLAlchemy
-- 14-model_city_fetch_by_state.py: fetch cities by state
+
+class City(Base):
+    """City class linked to cities table"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
